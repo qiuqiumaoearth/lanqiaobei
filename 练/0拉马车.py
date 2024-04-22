8d4c559c4541984b2502f5d0c84221ca90ea771f
qiuqiumaoearth <2016566910@qq.com>
2024-04-22 21:59:28 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ 练/0拉马车.py
@@ -0,0 +1,40 @@
+# a=list(input())
+# b=list(input())
+# zhuo=[]
+# while len(a)!=0 and len(b)!=0:
+#     a_c=a.pop(0)
+#     if a_c not in zhuo:
+#         zhuo.append(a_c)
+#     else:
+#         wz=zhuo.index(a_c)
+#         zhuo.append(a_c)
+#         c=zhuo[wz:]
+#         c.reverse()
+#         a=a+c
+#         zhuo=zhuo[:wz]
+#         e=a.pop(0)
+#         zhuo.append(e)
+#     print('a 的牌{}'.format(a))
+#     print('桌面上的牌{}'.format(zhuo))
+#     b_c=b.pop(0)
+#     if b_c not in zhuo:
+#         zhuo.append(b_c)
+#     else:
+#         wz = zhuo.index(b_c)
+#         zhuo.append(b_c)
+#         c = zhuo[wz:]
+#         c.reverse()
+#         b = b + c
+#         zhuo = zhuo[:wz]
+#         e = b.pop(0)
+#         zhuo.append(e)
+#     print('b 的牌{}'.format(b))
+#     print('桌面上的牌{}'.format(zhuo))
+# if len(a)!=0:
+#     print(zhuo[-1],a)
+# else:
+#     print(zhuo[-1],b)
+
+
+# K8XKA2A95A
+# 27K5J5Q6K4
